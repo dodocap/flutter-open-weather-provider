d1f5d2f9154792cdb1b23b199ea4f2230dc511a1
dodocap <dodocap@naver.com>
2024-03-04 19:20:10 +0900

    feat: Create Weather Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:open_weather_provider/pages/home_page.dart';
+import 'package:open_weather_provider/providers/weather/weather_provider.dart';
+import 'package:open_weather_provider/repositories/weather_repository.dart';
+import 'package:open_weather_provider/services/weather_api_services.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -12,14 +17,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Weather App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        Provider<WeatherRepository>(
+          create: (context) => WeatherRepository(
+            weatherApiServices: WeatherApiServices(
+              httpClient: http.Client(),
+            ),
+          ),
+        ),
+        ChangeNotifierProvider<WeatherProvider>(
+            create: (context) => WeatherProvider(
+              weatherRepository: context.read<WeatherRepository>(),
+            ),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Weather App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
