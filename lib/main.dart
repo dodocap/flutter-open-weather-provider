bb8e08e8a550bcef93c52dc36c36b159be957639
dodocap <dodocap@naver.com>
2024-03-04 18:43:41 +0900

    feat: Create Weather Repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:open_weather_provider/pages/home_page.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -12,11 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'OpenWeatherProvider',
+      title: 'Weather App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const HomePage(),
     );
   }
 }
