4f16563e3dac9e1c69205c910b499ee0d7cb7905
dodocap <dodocap@naver.com>
2024-03-05 18:35:13 +0900

    feat: Create SearchPage Widget - Form

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:open_weather_provider/pages/search_page.dart';
+import 'package:open_weather_provider/providers/providers.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +11,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String? _city;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Weather'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () async {
+              _city = await Navigator.push(context, MaterialPageRoute(builder: (context) => const SearchPage()));
+
+              if (_city != null && context.mounted) {
+                context.read<WeatherProvider>().fetchWeather(_city!);
+              }
+            },
+          )
+        ],
       ),
       body: Center(
         child: Text('Home'),
