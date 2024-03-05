10930aed266af8d79e6b2e29f366993024bb4911
dodocap <dodocap@naver.com>
2024-03-05 21:02:02 +0900

    feat: Create TempSettingsState, TempSettings ChangeNotifier and SettingsPage UI

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:open_weather_provider/providers/providers.dart';
+import 'package:provider/provider.dart';
 
-class SettingsPage extends StatefulWidget {
+class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
-  @override
-  State<SettingsPage> createState() => _SettingsPageState();
-}
-
-class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Settings'),
+        title: const Text('Settings'),
       ),
-      body: Center(
-        child: Text('Settings'),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListTile(
+          title: const Text('Temperature Unit'),
+          subtitle: const Text('Celsius/Fahrenheit (Default: Celsius)'),
+          trailing: Switch(
+            value: context.watch<TempSettingsProvider>().state.tempUnit == TempUnit.celsius,
+            onChanged: (_) => context.read<TempSettingsProvider>().toggleTempUnit(),
+          ),
+        ),
       ),
     );
   }
